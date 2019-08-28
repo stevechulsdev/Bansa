e4c17a20fba4e1f262eb2689cd2a22277604d196
sangchul <stevechulsdev@gmail.com>
2019-08-28 17:59:21 +0900

    add FCM and LocalPush

--- app/src/main/java/com/stevechulsdev/bansa/etc/Utils.kt
+++ app/src/main/java/com/stevechulsdev/bansa/etc/Utils.kt
@@ -1,6 +1,16 @@
 package com.stevechulsdev.bansa.etc
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.stevechulsdev.bansa.R
+import com.stevechulsdev.bansa.main.view.MainActivity
 import java.lang.Exception
 
 object Utils {
@@ -53,5 +63,37 @@ object Utils {
 
     fun setLocalUserDataIsLogin(context: Context, value: Boolean) {
         setLocalUserDataBoolean(context, Constants.LOCAL_DATA_KEY_USER_IS_LOGIN, value)
+    }
+
+    fun sendLocalPush(context: Context, title: String, contents: String) {
+        // Pending Intent 부분은 목적에 맞게 사용해야 됨
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            NotificationChannel("localChannel", "localPush", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = ""    // 채널에 대한 설명
+                enableLights(true)
+                enableVibration(true)
+                setShowBadge(true)
+                vibrationPattern = longArrayOf(100, 200, 100, 200)
+                notificationChannel.createNotificationChannel(this)
+            }
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(context, "localChannel")
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(title)
+            .setContentText(contents)
+            .setAutoCancel(true)
+            .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+            .setContentIntent(pendingIntent)
+
+        (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).apply {
+            notify(0, notificationBuilder.build())
+        }
     }
 }
